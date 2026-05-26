d99386e52b06ac75fc61d7ed32a8d6c9f3992e60
sriramcse2102-ship-it <sriramcse2102@gmail.com>
2026-05-26 16:05:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Instant Booking Sports App</h1>
+
+      <div className="card">
+        <h2>Football Turf</h2>
+        <p>Location: Chennai</p>
+        <p>Available Slot: 6 PM - 7 PM</p>
+        <button>Book Now</button>
+      </div>
+
+      <div className="card">
+        <h2>Cricket Ground</h2>
+        <p>Location: Chennai</p>
+        <p>Available Slot: 7 PM - 8 PM</p>
+        <button>Book Now</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
